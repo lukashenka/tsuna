e0245ff3d55e71356200ee15a2becff2ae62973a
e.karachun <Dgecnbvtyzdrjvg90>
2016-04-24 14:48:33 +0300

    hui

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -79,6 +79,14 @@ AppAsset::register($this);
 
 <!-- Footer -->
 <footer>
+    <script type="text/javascript" src="//vk.com/js/api/openapi.js?121"></script>
+
+    <!-- VK Widget -->
+    <div id="vk_groups"></div>
+    <script type="text/javascript">
+        VK.Widgets.Group("vk_groups", {mode: 0, width: "1900", height: "40", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 86495283);
+    </script>
+
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
